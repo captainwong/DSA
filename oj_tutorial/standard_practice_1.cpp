4bd75b34f2305bc7ad1ab73a1f12851713d7c65b
Jack-PC-SSD <1281261856@qq.com>
2014-09-23 22:51:22 +0800

    uncomment another main()

--- oj_tutorial/standard_practice_1.cpp
+++ oj_tutorial/standard_practice_1.cpp
@@ -4,17 +4,17 @@ static inline bool is_valid_value(int val)
 {
 	return val >= 1 && val <= 10000000;
 }
-//
-//int main(int argc, char* argv[])
-//{
-//	int a = 0;
-//	int b = 0;
-//	//std::cout << "Enter 2 positive integers:" << std::endl;
-//	std::cin >> a >> b;
-//	if (!is_valid_value(a) || !is_valid_value(b)) {
-//		std::cout << "You must input 2 positive integers." << std::endl;
-//	} else {
-//		std::cout << a + b << std::endl;
-//	}
-//	return 0;
-//}
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 0;
+	//std::cout << "Enter 2 positive integers:" << std::endl;
+	std::cin >> a >> b;
+	if (!is_valid_value(a) || !is_valid_value(b)) {
+		std::cout << "You must input 2 positive integers." << std::endl;
+	} else {
+		std::cout << a + b << std::endl;
+	}
+	return 0;
+}
