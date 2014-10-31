bfe0c3b3f087d6cb58cd6cfc9e56f98647132743
Jack-PC-SSD <1281261856@qq.com>
2014-11-01 01:06:11 +0800

    hospital 90        thu pa1 all 90

--- Hospital/hospital.cpp
+++ Hospital/hospital.cpp
@@ -69,48 +69,64 @@ int main(int argc, char* argv[])
 	if (scanf("%d", &n) == EOF)
 		return 0;
 
-	VILLAGE* v = new VILLAGE[n + 1];
+	//VILLAGE* v = new VILLAGE[n + 1];
 	//longlong *Ewd = new longlong[n + 1];
-	longlong Ewd0 = 0;
+	longlong EWD_zero = 0;
 	longlong Ewn = 0;
 	//Ewd[0] = 0;
-	v[0].x = v[0].w = 0;
-	for (int i = 1; i <= n; i++) {
-		if (scanf("%d %d", &v[i].x, &v[i].w) == EOF)
+	longlong v[32768] = { 0 };
+	//v[0].x = v[0].w = 0;
+	int x, w;
+	int minimize_index = 32768;
+	for (int i = 0; i < n; i++) {
+		if (scanf("%d %d", &x, &w) == EOF)
 			return 0;
-		Ewd0 += (longlong)v[i].x * (longlong)v[i].w;
+		minimize_index = minimize_index < x ? minimize_index : x;
+		//if (v[x] < w)
+			v[x] += w;
+		EWD_zero += (longlong)x * (longlong)w;
 		//EwdN += wd[i];
-		Ewn += v[i].w;
+		Ewn += w;
 	}
 
 #ifndef _OJ_
-	print_vellage(v, 1, n + 1);
+	//print_vellage(v, 1, n + 1);
 #endif
 
-	quik_sort(v, 1, n);
+	//quik_sort(v, 1, n);
 
 #ifndef _OJ_
-	print_vellage(v, 1, n + 1);
+	//print_vellage(v, 1, n + 1);
 #endif
 
-	longlong Ewd1 = Ewd0 - v[1].x * Ewn;
-	int minimize_index = 1;
-	longlong prev_ewdn = Ewd1;
-	longlong minimize_ewdn = Ewd1;
-	longlong Ewn_prev = v[1].w;
-	for (int i = 2; i <= n; i++) {
+	longlong Ewd_first = 0;
+	if (minimize_index == 0) {
+		Ewd_first = EWD_zero;
+	} else {
+		Ewd_first = EWD_zero - minimize_index * Ewn;
+	}
+	
+	int prev_index = minimize_index;
+	longlong prev_ewdn = Ewd_first;
+	longlong minimize_ewdn = Ewd_first;
+	longlong Ewn_prev = v[minimize_index];
+	for (int i = minimize_index + 1; i < 32768; i++) {
+		if (v[i] != 0) {
+			longlong ewdi = prev_ewdn + 2 * (i - prev_index) * Ewn_prev
+				+ (prev_index - i) * Ewn;
+			prev_ewdn = ewdi;
+			Ewn_prev += v[i];
+			if (ewdi < minimize_ewdn) {
+				minimize_index = i;
+				minimize_ewdn = ewdi;
+			}
+			prev_index = i;
+		}
 		//longlong Ewdi = abs(EwdN - static_cast<longlong>(vellages[i].x) * EWn - 2 * Ewd[i - 1]);
 		// Ewn is exactly the prev Ewn
-		longlong ewdi = prev_ewdn + 2 * (v[i].x - v[i - 1].x) * Ewn_prev 
-			+ (v[i - 1].x - v[i].x) * Ewn;
-		prev_ewdn = ewdi;
-		Ewn_prev += v[i].w;
-		if (ewdi < minimize_ewdn) {
-			minimize_index = i;
-			minimize_ewdn = ewdi;
-		}
+		
 	}
-	printf("%d\n", v[minimize_index].x);
+	printf("%d\n", minimize_index);
 	printf("%lld\n", minimize_ewdn);
 
 	return 0;
