54ab2cda6e638956199c75a6a42f99fdeb30c92d
win10_i5-4590 <1281261856@qq.com>
2018-08-09 21:26:23 +0800

    使用排列组合，先算逆序对数量，95通过

--- PA/PA1_3_LightHouse/main180808.cpp
+++ PA/PA1_3_LightHouse/main180808.cpp
@@ -33,8 +33,12 @@ Light TmpLights[(MAX_LIGHT >> 1) + 1] = {};
 
 void mergeSortX(Light* lights, int lo, int hi);
 void mergeX(Light* lights, int lo, int mi, int hi);
+
 long sequenceYInside(Light* lights, int lo, int hi);
 long sequenceYBetween(Light* lights, int lo, int mi, int hi);
+
+long invertionYInside(Light* lights, int lo, int hi);
+long invertionYBetween(Light* lights, int lo, int mi, int hi);
 
 int main()
 {
@@ -48,12 +52,24 @@ int main()
 	}
 
 	mergeSortX(Lights, 0, n);
-	auto sum = sequenceYInside(Lights, 0, n);
+	// 顺序对解法
+	//auto sum = sequenceYInside(Lights, 0, n);
+
+	// 2018-8-9 21:19:23 
+	// 逆序对解法
+	// 若计算出逆序对数量，则 顺序对数量 = 所有灯塔两两组合的总数 - 逆序对数量
+	// 所有灯塔两两组合的总数。怎么求呢？高中数学老师的棺材板快按不住了！！！
+	// 明显为 C(n, 2)
+	// = A(n, 2) / 2
+	// = n! / (n - 2)! / 2
+	// = n(n-1) / 2
+	auto sum = invertionYInside(Lights, 0, n);
+	sum = (long)n * (n - 1) / 2 - sum;
+
+
 	printf("%ld\n", sum);
 
 #ifndef _OJ_
-	//getchar(); getchar(); getchar(); getchar(); getchar(); getchar(); getchar(); getchar(); getchar();
-	//_getw(stdin);
 	system("pause");
 #endif // !_OJ_
 
@@ -108,10 +124,6 @@ long sequenceYBetween(Light * lights, int lo, int mi, int hi)
 	auto lb = mi - lo;
 	auto lc = hi - mi;
 
-	/*for (int i = 0; i < lb; i++) {
-	B[i] = A[i];
-	}*/
-
 	memcpy(B, A, lb * sizeof(Light));
 
 	long sum = 0;
@@ -139,6 +151,46 @@ long sequenceYBetween(Light * lights, int lo, int mi, int hi)
 
 		if ((ic < lc) && (!(ib < lb) || (C[ic].y < B[ib].y))) { 
 			A[ia++] = C[ic++]; 
+		}
+	}
+
+	return sum;
+}
+
+long invertionYInside(Light * lights, int lo, int hi)
+{
+	if (hi - lo < 2) { return 0; }
+	int mi = (lo + hi) / 2;
+	auto a = invertionYInside(lights, lo, mi);
+	auto b = invertionYInside(lights, mi, hi);
+	auto c = invertionYBetween(lights, lo, mi, hi);
+	return a + b + c;
+}
+
+long invertionYBetween(Light * lights, int lo, int mi, int hi)
+{
+	auto A = lights + lo;
+	auto B = TmpLights;
+	auto C = lights + mi;
+
+	auto lb = mi - lo;
+	auto lc = hi - mi;
+
+	memcpy(B, A, lb * sizeof(Light));
+
+	long sum = 0;
+
+	for (int ia = 0, ib = 0, ic = 0; (ib < lb) || (ic < lc); ) {
+		if ((ib < lb) && (!(ic < lc) || (B[ib].y < C[ic].y))) {
+			A[ia++] = B[ib++];
+		}
+
+		if ((ic < lc) && (!(ib < lb) || (C[ic].y < B[ib].y))) {
+			if (ib < lb) {
+				sum += lb - ib;
+			}
+
+			A[ia++] = C[ic++];
 		}
 	}
 
