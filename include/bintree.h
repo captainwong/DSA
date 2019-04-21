b0cdec2eff0b2084d107a03ef4d665e9b7bdf954
captainwong <1281261856@qq.com>
2019-04-21 16:05:48 +0800

    update

--- include/bintree.h
+++ include/bintree.h
@@ -19,17 +19,13 @@ protected:
 	//! 更新节点 node 的高度
 	virtual int updateHeight(NodePtr node) {
 		// return node->height_ = 1 + std::max(stature(node->lChild_), stature(node->rChild_));
-		auto lh = stature(node->lChild_);
-		auto rh = stature(node->rChild_);
+		auto lh = stature(node->lChild_); auto rh = stature(node->rChild_);
 		return node->height_ = 1 + (lh < rh ? rh : lh);
 	}
 
 	//! 更新节点 node 及其祖先的高度
 	void updateHeightAbove(NodePtr node) {
-		while (node) {
-			updateHeight(node);
-			node = node->parent_;
-		}
+		while (node) { updateHeight(node); node = node->parent_; }
 	}
 
 public:
@@ -50,36 +46,26 @@ public:
 	NodePtr insertAsRootNode(T const& data) { size_ = 1; return (root_ = new Node(data)); }
 
 	NodePtr insertAsLeftChild(NodePtr node, T const& data) {
-		size_++;
-		node->insertAsLeftChild(data);
-		updateHeightAbove(node);
+		size_++; node->insertAsLeftChild(data); updateHeightAbove(node);
 		return node->lChild_;
 	}
 
 	NodePtr insertAsRightChild(NodePtr node, T const& data) {
-		size_++;
-		node->insertAsRightChild(data);
-		updateHeightAbove(node);
+		size_++; node->insertAsRightChild(data); updateHeightAbove(node);
 		return node->rChild_;
 	}
 
 	template <typename VST>
 	static void visitAlongLeftBranch(NodePtr node, VST& visit, Stack<NodePtr>& stack) {
-		while (node) {
-			visit(node->data_);
-			stack.push(node->rChild_);
-			node = node->lChild_;
-		}
+		while (node) { visit(node->data_); stack.push(node->rChild_); node = node->lChild_; }
 	}
 
 	//! 先序遍历
 	template <typename VST>
 	void travPreOrder(NodePtr node, VST& visit) {
 		Stack<NodePtr> stack;
-		while (true) {
-			visitAlongLeftBranch(node, visit, stack);
-			if (stack.empty()) { break; }
-			node = stack.pop();
+		while (true) { visitAlongLeftBranch(node, visit, stack); 
+			if (stack.empty()) { break; } node = stack.pop();
 		}
 	}
 
@@ -91,12 +77,9 @@ public:
 	template <typename VST>
 	void travInOrder(NodePtr node, VST& visit) {
 		Stack<NodePtr> stack;
-		while (true) {
-			goAlongLeftBranch(node, stack);
-			if (stack.empty()) { break; }
-			node = stack.pop();
-			visit(node->data_);
-			node = node->rChild_;
+		while (true) { goAlongLeftBranch(node, stack);
+			if (stack.empty()) { break; } node = stack.pop();
+			visit(node->data_); node = node->rChild_;
 		}
 	}
 
@@ -120,8 +103,7 @@ public:
 		if (node) { stack.push(node); }
 		while (!stack.empty()) {
 			if (stack.top() != node->parent_) { goToHLVFL(stack); }
-			node = stack.pop();
-			visit(node->data_);
+			node = stack.pop(); visit(node->data_);
 		}
 	}
 
@@ -131,8 +113,7 @@ public:
 		Queue<NodePtr> queue;
 		queue.enqueue(node);
 		while (!queue.empty()) {
-			node = queue.dequeue();
-			visit(node->data_);
+			node = queue.dequeue(); visit(node->data_);
 			if (node->lChild_) { queue.enqueue(node->lChild_); }
 			if (node->rChild_) { queue.enqueue(node->rChild_); }
 		}
