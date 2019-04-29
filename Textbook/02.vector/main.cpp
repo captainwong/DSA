05d50447bd1cd8f61e1229aa0529954313e03f40
captainwong <1281261856@qq.com>
2019-04-29 18:07:02 +0800

    update

--- Textbook/02.vector/main.cpp
+++ Textbook/02.vector/main.cpp
@@ -6,49 +6,6 @@ using namespace dtl;
 
 int main(int argc, char* argv[])
 {
-	/*Fibnacci f(0);
-
-	for (int i = 0; i < 100; i++, f.next()) {
-		printf("Fib(%05d) = %22I64d\n", i, f.get());
-	}*/
-
-	Vector<int> vector;
-	vector.insert(1);
-	vector.insert(3);
-	vector.insert(2);
-	vector.insert(4);
-	vector.insert(5);
-	vector.insert(1);
-	vector.insert(1);
-
-	int size = vector.size();
-	printf("Initialize:\n");
-	for (int i = 0; i < size; i++) {
-		printf("%d ", vector[i]);
-	}
-	printf("\n");
-
-	vector.sort();
-	printf("Sorted:\n");
-	for (int i = 0; i < size; i++) {
-		printf("%d ", vector[i]);
-	}
-	printf("\n");
-
-	
-	vector.uniquify();
-	size = vector.size();
-	printf("Uniquified:\n");
-	for (int i = 0; i < size; i++) {
-		printf("%d ", vector[i]);
-	}
-	printf("\n");
-
-	getchar();
-
-	system("cls");
-
 	test::testVector<int>(10, 1000);
-
 	return 0;
 }
