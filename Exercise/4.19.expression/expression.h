2bd4a40c68e3df81b73798b1e7890cdf0c3eb6a2
captainwong <1281261856@qq.com>
2019-05-05 21:53:01 +0800

    working on 4.19

---
+++ Exercise/4.19.expression/expression.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../../include/dtl/stack.h"
+
+// a) Ö§³Ö + *
+static void program_a(int S)
+{
+	dtl::Stack<int> opnd;
+	for (int i = 9; i >= 0; i--) {
+		opnd.push(i);
+	}
+
+	auto giveBack = [&opnd](int n) {
+		int m = n % 10;
+		n /= 10;
+		opnd.push(m);
+		return n;
+	};
+
+	auto a = opnd.pop();
+
+	while (!opnd.empty()) {
+		char optrs[] = "+*";
+		for (auto optr : optrs) {
+			if (optr == '+') {
+
+			} else {
+
+			}
+		}
+	}
+}
